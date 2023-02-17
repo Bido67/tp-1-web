8561b1d0e1230a4412d885d2f0b35e2a78e9e19d
Ahiruneko-Chan <63516930+Bido67@users.noreply.github.com>
2023-02-17 07:44:24 -0500

    Updated :3
    uwu

--- js/prepareSearch.js
+++ js/prepareSearch.js
@@ -1,4 +1,4 @@
-function preparePage(){
+async function preparePage(){
     /*for (const [key, value] of mySearchParams) {
 
     }*/
@@ -37,7 +37,7 @@ function preparePage(){
     }
     else{
         document.querySelectorAll(`input[type="checkbox"]`).forEach((checkbox) => {checkbox.checked = true;})
-        editPage();
+        setTimeout(editPage, 500);
     }
    /*
     if(brands){
